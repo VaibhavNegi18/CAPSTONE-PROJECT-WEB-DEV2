c603b8532a774d0669c6b9c9e1dd2df52c97b7ee
Vaibhav Negi <vaibhav@example.com>
2026-05-03 22:23:00 +0530

    this

--- src/mock/data.js
+++ src/mock/data.js
@@ -4,14 +4,14 @@ export const products = [
     name: "iPhone 14",
     price: 999,
     description: "Latest iPhone model",
-    picture: "https://dummyjson.com/image/i/products/1/thumbnail.jpg"
+    picture: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
   },
   {
     _id: "2",
     name: "Nike Shoes",
     price: 199,
     description: "Comfortable running shoes",
-    picture: "https://dummyjson.com/image/i/products/2/thumbnail.jpg"
+    picture: "https://images.unsplash.com/photo-1542291026-7eec264c27ff"
   }
 ];
 
@@ -19,11 +19,11 @@ export const categories = [
   {
     _id: "1",
     name: "Electronics",
-    picture: "https://dummyjson.com/image/i/products/1/thumbnail.jpg"
+    picture: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
   },
   {
     _id: "2",
     name: "Fashion",
-    picture: "https://dummyjson.com/image/i/products/2/thumbnail.jpg"
+    picture: "https://images.unsplash.com/photo-1542291026-7eec264c27ff"
   }
 ];
